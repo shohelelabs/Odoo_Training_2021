a80b4ce0faff9d9058aac4657e61e8c574e1ce80
shohelelabs <shohel.mahmud@elabs.de>
2021-01-07 11:15:29 +0000

    [ADD] menu items

--- odoo_library/__manifest__.py
+++ odoo_library/__manifest__.py
@@ -1,21 +1,22 @@
 #-*- coding: utf-8 -*-
 {
     'name': 'Odoo Library',
-    'Summary':"""Academy app to manage Training""",
+    'Summary':"""Library app to manage organizational books""",
     'description':"""
-        Academy module to manage Training:
+        Library module to manage learning and Training:
         - Course
         - Sessions
         - Attendees
     """,
     'author':'Elabs',
     'website':'https://www.elabs.de',
-    'category': 'Training',
+    'category': 'Learning and Training',
     'version':'0.1',
     'depends':['base'],
     
     'data':[
-        'views/book_views.xml'
+        'views/book_views.xml',
+        'views/library_menuitems.xml',
     ],
     
     'demo': [
