ae69c76ae598478d78003d648356487f482b70fc
shohelelabs <shohel.mahmud@elabs.de>
2021-01-12 11:22:50 +0000

    Add advanced view

--- odoo_library/models/reservation.py
+++ odoo_library/models/reservation.py
@@ -1,6 +1,7 @@
 # *- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from datetime import timedelta
 
 class Reservation(models.Model):
     _name = 'library.reservation'
@@ -9,5 +10,30 @@ class Reservation(models.Model):
     book_id=fields.Many2one(comodel_name='library.book',string='Book', ondelete='cascade', required=True)
     
     name=fields.Char(string='Title',related='book_id.name')
+    
     reservation_operator_id = fields.Many2one(comodel_name='res.partner',string='Reservation Operator')
     member_ids = fields.Many2many(comodel_name='res.partner',string='Reservation Member')
+    
+    
+    start_date = fields.Date(string='Start Date', default=fields.Date.today)
+    
+    duration = fields.Integer(string='Reservation Days', default=1)
+    
+    end_date = fields.Date(string='End Date',compute='_compute_end_date',inverse='_inverse_end_date',store=True)
+    
+    
+    @api.depends('start_date','duration')
+    def _compute_end_date(self):
+        for record in self:
+            if not (record.start_date and record.duration):
+                record.end_date = record.start_date
+            else:
+                duration = timedelta(days=record.duration)
+                record.end_date = record.start_date + duration
+                
+    def _inverse_end_date(self):
+        for record in self:
+            if record.start_date and record.end_date:
+                record.duration = (record.end_date - record.start_date).days + 1
+            else:
+                continue
