76f32031992f1f24d92c4ba3a7c13769c37dbb2e
shohelelabs <shohel.mahmud@elabs.de>
2021-01-24 20:04:12 +0000

    bug fixing in controllers2

--- odoo_library/controllers/.ipynb_checkpoints/library_controllers-checkpoint.py
+++ odoo_library/controllers/.ipynb_checkpoints/library_controllers-checkpoint.py
@@ -11,8 +11,8 @@ class Library(http.controller):
     @http.route('/library/books/',auth='public',website=True)
     def books(self, **kw):
         books = http.request.env['library.book'].serch([])
-        return http.request.render('odoo_library.book_website',{'books': books})
+        return http.request.render('odoo_library.book_website',{'books': books,})
     
     @http.route('/library/<model("library.reservation"):reservation>/',auth='public',website=True)
     def reservation(self,reservation):
-    return http.request.render('odoo_library.reservation_website',{'reservation':reservation})
+        return http.request.render('odoo_library.reservation_website',{'reservation':reservation})
