0cbbc2c4be36c890a17f8e5b5bd2fb92a0f9d743
shohelelabs <shohel.mahmud@elabs.de>
2021-01-25 09:53:57 +0000

    Bug fixing 3

--- odoo_library/controllers/library_controllers.py
+++ odoo_library/controllers/library_controllers.py
@@ -10,7 +10,7 @@ class Library(http.Controller):
     
     @http.route('/library/books/',auth='public',website=True)
     def books(self, **kw):
-        books = http.request.env['library.book'].serch([])
+        books = http.request.env['library.book'].search([])
         return http.request.render('odoo_library.book_website',{'books': books,})
     
     @http.route('/library/<model("library.reservation"):reservation>/',auth='public',website=True)
